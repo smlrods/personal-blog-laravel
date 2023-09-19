953cad2b6852a269ebfd12f0f004279bb5308522
smlrods <smlrodrigues.dev@gmail.com>
2023-09-19 15:34:30 -0300

    Initiate database migrations for schema setup.

---
+++ database/migrations/2023_09_19_130930_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->string('full_text')->nullable(false);
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
