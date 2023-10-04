4d75cf3ca66424d1fbd96c54715d00e0ca2400b3
smlrods <smlrodrigues.dev@gmail.com>
2023-10-04 09:18:07 -0300

    Add application Eloquent Models

---
+++ app/Models/Article.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'full_text',
+        'image',
+        'category_id',
+    ];
+
+    public function getLinkAttribute() {
+        return '/admin/articles/' . $this->attributes['id'];
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'article_tags');
+    }
+}
