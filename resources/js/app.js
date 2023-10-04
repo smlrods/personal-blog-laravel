e876b10aa70926d44f0025f8bf5421383559bf0e
smlrods <smlrodrigues.dev@gmail.com>
2023-10-04 09:22:55 -0300

    Add Form `js` logic

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,44 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import Editor from '@toast-ui/editor'
+import '@toast-ui/editor/dist/toastui-editor.css';
+
 window.Alpine = Alpine;
 
 Alpine.start();
+
+let editorElement = document.querySelector('#editor');
+
+let editor;
+
+if (editorElement) {
+  editor = new Editor({
+    el: editorElement,
+    height: '400px',
+    initialEditType: 'markdown',
+    placeholder: 'Write something cool!',
+  })
+}
+
+const createForm = document.querySelector('#createArticleForm');
+const editForm = document.querySelector('#editArticleForm');
+
+if (createForm) {
+  document.querySelector('#createArticleForm').addEventListener('submit', e => {
+    e.preventDefault();
+    document.querySelector('#full_text').value = editor.getMarkdown();
+    e.target.submit();
+  });
+}
+
+if (editForm) {
+  let editorInput = document.querySelector('#full_text');
+  editor.setMarkdown(document.querySelector('#full_text').value);
+
+  editForm.addEventListener('submit', e => {
+    e.preventDefault();
+    document.querySelector('#full_text').value = editor.getMarkdown();
+    e.target.submit();
+  });
+}
