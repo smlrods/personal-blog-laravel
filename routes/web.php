cceee27fdf4b0c0989f5eeb283d51ec0096c3a6d
smlrods <smlrodrigues.dev@gmail.com>
2023-10-04 09:23:40 -0300

    Add application routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\ArticlesController;
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TagsController;
+use App\Models\Article;
 use Illuminate\Support\Facades\Route;
+
+use function PHPUnit\Framework\isEmpty;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +21,47 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $articles = Article::all();
+
+    return view('home', ['articles' => $articles]);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('articles/{id}', function ($id) {
+    $article = Article::find($id);
 
-Route::middleware('auth')->group(function () {
+    if (!$article) {
+        return redirect('/');
+    }
+
+    return view('item', ['article' => $article]);
+})->name('articles');
+
+Route::redirect('/articles', '/');
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/admin', function () {
+    $items = [
+        (object) ['name' => 'Manage categories', 'link' => route('admin.categories.index')],
+        (object) ['name' => 'Manage tags', 'link' => route('admin.tags.index')],
+        (object) ['name' => 'Manage articles', 'link' => route('admin.articles.index')]
+    ];
+
+    return view('dashboard', ['items' => $items, 'header' => 'Dashboard']);
+})->middleware(['auth', 'verified'])->name('admin');
+
+Route::middleware('auth')->prefix('/admin')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('admin/categories', CategoriesController::class)->names('admin.categories');
+
+Route::resource('admin/tags', TagsController::class)->names('admin.tags');
+
+Route::resource('admin/articles', ArticlesController::class)->names('admin.articles');
 
 require __DIR__.'/auth.php';
